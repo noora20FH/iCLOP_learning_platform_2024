3f9a140d6819403b3fad5c56d2ba3b62a24b723f
NOORA AULIA HIDAYAT <73746751+noora20FH@users.noreply.github.com>
2025-03-27 11:39:15 +0700

    Delete TestPython directory

--- TestPython/answer_chapter1_experiments1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-url="https://raw.githubusercontent.com/noora20FH/skripsi_noora2023/main/purchases.csv"
-
-def data_load():
-  df =pd.read_csv(url)
-  return df
-
-print(data_load())
